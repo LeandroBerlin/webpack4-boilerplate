d593d078fb2a760736da892062df54f1a63144b4
Leandro <leandro.frigerio@devugees.org>
2019-03-10 12:26:42 +0100

    Clenup

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     filename: "assets/js/bundle.js",
     publicPath: ""
   },
+  devtool: 'source-map',
   optimization: {
     minimizer: [new UglifyJsPlugin(), new OptimizeCSSAssetsPlugin()]
   },
