7c2701ba5490a701c623b01855595597c2d6d8a3
Leandro DCI <leandro.frigerio@digitalcareerinstitute.org>
2021-04-15 16:00:13 +0200

    fix: configuration & deps

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,24 +1,27 @@
-import "@scss/styles.scss";
-import logoImg from "@img/logo.png";
-
-
-window.addEventListener('click', (ev) => {
-    const elm = ev.target;
-    const selector = elm.getAttribute('data-target');
-    collapse(selector, 'toggle');
-}, false);
+import '@scss/styles.scss';
+import logoImg from '@img/logo.png';
 
 const fnmap = {
-    'toggle': 'toggle',
-    'show': 'add',
-    'hide': 'remove'
+    toggle: 'toggle',
+    show: 'add',
+    hide: 'remove',
 };
 const collapse = (selector, cmd) => {
     document.querySelector(selector).classList[fnmap[cmd]]('show');
-}
+};
 
-let filename = logoImg.substring(logoImg.lastIndexOf('/') + 1);
-logo.src = `assets/img/${filename}`;
+window.addEventListener(
+    'click',
+    (ev) => {
+        const elm = ev.target;
+        const selector = elm.getAttribute('data-target');
+        collapse(selector, 'toggle');
+    },
+    false,
+);
 
-let message = "Hello Webpack";
+const filename = logoImg.substring(logoImg.lastIndexOf('/') + 1);
+const logo = `assets/img/${filename}`;
+console.log(logo);
+const message = 'Hello Webpack';
 console.log(` Message is: ${message}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,91 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const TerserPlugin = require("terser-webpack-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  entry: "./src/assets/js/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist/"),
-    filename: "assets/js/bundle.js",
-    publicPath: "./"
-  },
-  devtool: 'source-map',
-  optimization: {
-    minimizer: [new TerserPlugin(), new CssMinimizerPlugin()]
-  },
-  module: {
-    rules: [
-      //babel
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"]
-        }
-      },
-      //style and css extract
-      {
-        test: [/.css$|.scss$/],
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-
-      },
-      //image file loader
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              name: "[name].[ext]",
-              outputPath: "assets/img/",
-
-            }
-          }
-        ]
-      },
-      //fonts
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name: '[name].[ext]',
-            outputPath: 'assets/fonts/',
-          }
-        }]
-      }
-    ]
-  },
-  resolve: {
-    alias: {
-      '@scss': path.resolve(__dirname, 'src/assets/scss'),
-      '@img': path.resolve(__dirname, 'src/assets/img'),
-      '@': path.resolve(__dirname, 'src')
+    entry: './src/assets/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        filename: 'assets/js/bundle.js',
+        publicPath: './',
     },
-    modules: [
-      'node_modules',
-      path.resolve(__dirname, 'src')
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "assets/css/styles.css"
-    }),
-    new HtmlWebpackPlugin({
-      title: "Setting up webpack 4",
-      template: "src/index.html",
-      inject: true,
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true
-      }
-    }),
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['dist'] }
-    })
-  ],
-  performance: {
-    maxEntrypointSize: 1000000,
-    maxAssetSize: 1000000
-  }
+    devtool: 'source-map',
+    optimization: {
+        minimizer: [new TerserPlugin(), new CssMinimizerPlugin()],
+    },
+    module: {
+        rules: [
+            // babel
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader'],
+            },
+            // style and css extract
+            {
+                test: [/.css$|.scss$/],
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            // image file loader
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/img/',
+                        },
+                    },
+                ],
+            },
+            // fonts
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'assets/fonts/',
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            '@scss': path.resolve(__dirname, 'src/assets/scss'),
+            '@img': path.resolve(__dirname, 'src/assets/img'),
+            '@': path.resolve(__dirname, 'src'),
+        },
+        modules: ['node_modules', path.resolve(__dirname, 'src')],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'assets/css/styles.css',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Setting up webpack 4',
+            template: 'src/index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+            },
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: { baseDir: ['dist'] },
+        }),
+    ],
+    performance: {
+        maxEntrypointSize: 1000000,
+        maxAssetSize: 1000000,
+    },
 };
